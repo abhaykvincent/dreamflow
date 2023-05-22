574d927956cdd8fdcc62d12cea10a632f4890198
Abhay <abhaykvincent@gmail.com>
2023-05-22 12:21:33 -0400

    Initialized with react, redux- typescript with SCSS

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,11 +5,11 @@ import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
+  const { container } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  const appDiv = container.querySelector('.App');
+  expect(appDiv).toBeInTheDocument();
 });
