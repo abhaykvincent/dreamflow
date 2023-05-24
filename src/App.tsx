fc0c9d09ae9cb6148f7c2b3873f5da113a0496ee
Abhay <abhaykvincent@gmail.com>
2023-05-24 09:53:30 -0400

    Sidebar toggle functionality
    with switch annimation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [activeSidebar, setActiveSidebar] = useState('layers');
+
+  function sidebarToolSelector(tool:string){
+    setActiveSidebar(tool)
+  }
   return (
     <div className="App">
     {/* Menu bar */}
@@ -22,15 +27,40 @@ function App() {
       {/* Sidebar */}
       <div className="sidebar">
         <div className="tools">
-          <div className="quick-tool element"></div>
-          <div className="quick-tool layers"></div>
+          <div className={`quick-tool element ${activeSidebar == 'elements' ? 'active':''}`}
+            onClick={()=>sidebarToolSelector('elements')}
+          ></div>
+          <div className={`quick-tool layers ${activeSidebar == 'layers' ? 'active':''}`}
+            onClick={()=>sidebarToolSelector('layers')}
+            ></div>
           <div className="quick-tool pages"></div>
           <div className="quick-tool assets"></div>
         </div>
         <div className="side-menu">
-          <div className="elements">
+
+          <div className={`side-panel elements ${activeSidebar == 'elements' ? '':'hide'}`}>
             <div className="tabs">Elements</div>
+            <div className="elements-dragabble">
+
+              <div className="element h">
+                <div className="element_icon"></div>
+                <div className="element_label">Heading</div>
+              </div>
+              <div className="element p">
+                <div className="element_icon"></div>
+                <div className="element_label">Paragraph</div>
+              </div>
+              <div className="element a">
+                <div className="element_icon"></div>
+                <div className="element_label">Link</div>
+              </div>
+
+            </div>
           </div>
+          <div className={`side-panel layers ${activeSidebar == 'layers' ? '':'hide'}`}>
+            <div className="tabs">Layers</div>
+          </div>
+
         </div>
       </div>
       {/* Canvas */}
