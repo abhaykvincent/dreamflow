62fff9ba2f40970b49ca1af6dac989dd5c0eeb4c
Abhay <abhaykvincent@gmail.com>
2023-05-26 22:20:18 -0400

    Header  center actions button added (Export)

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -7,8 +7,25 @@ export function Header() {
         <div className="left">
           <div className="logo"></div>
         </div>
-        <div className="center">c</div>
-        <div className="right">r</div>
+        <div className="center">
+          <div className="responsive">
+            <div className="devices_wrap">
+              <div className="highlighter"></div>
+              <div className="devices">
+                <div className="device">a</div>
+                <div className="device">a</div>
+                <div className="device">a</div>
+                <div className="device">a</div>
+              </div>
+            </div>
+          </div>
+          <div className="role"></div>
+          <div className="actions">
+            <div className="button">Export</div>
+          </div>
+        </div>
+        <div className="right">
+        </div>
       </header>
   );
 }
