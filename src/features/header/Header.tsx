2255bd50c4cdce428fcdffb8d83045a7bc5c9e4a
Abhay <abhaykvincent@gmail.com>
2023-05-28 16:10:13 -0400

    Role tab in header  created

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -4,11 +4,16 @@ import './Header.scss';
 export function Header() {
 
   const devices = ['a', 'b', 'c', 'd']; // An array of labels
+  const roles = ['Design','Code']; // An array of labels
 
   const [highlightedDevice, setHighlightedDevice] = useState('b');
+  const [highlightedRole, setHighlightedRole] = useState('Design');
 
   const toggleHighlight = (device:string) => {
     setHighlightedDevice(device);
+  };
+  const toggleRole = (role:string) => {
+    setHighlightedRole(role);
   };
 
   return (
@@ -42,7 +47,20 @@ export function Header() {
               <div className="unit">1920 px</div>
             </div>
           </div>
-          <div className="role"></div>
+          <div className="roles">
+            <div className="tab-button">
+              <div className={`highlighter ${highlightedRole.toLowerCase()}`}></div>
+              <div className="tabs">
+              {
+                roles.map(role=>(
+                  <div key={role} className={`role ${role.toLowerCase()} ${ (role===highlightedRole?'highlighted':'')} `}
+                  onClick={() => toggleRole(role)}
+                  >{role}</div>
+                ))
+              }
+              </div>
+            </div>
+          </div>
           <div className="actions">
             <div className="button">Export</div>
           </div>
