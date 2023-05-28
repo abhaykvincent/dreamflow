b75b0d302c2045a14a4fbb9d32ca766363f7460a
Abhay <abhaykvincent@gmail.com>
2023-05-28 15:40:15 -0400

    Version-conttrol UI created

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -2,21 +2,44 @@ import React, { useState } from 'react';
 import './Header.scss';
 
 export function Header() {
+
+  const devices = ['a', 'b', 'c', 'd']; // An array of labels
+
+  const [highlightedDevice, setHighlightedDevice] = useState('b');
+
+  const toggleHighlight = (device:string) => {
+    setHighlightedDevice(device);
+  };
+
   return (
     <header>
         <div className="left">
           <div className="logo"></div>
+          <div className="page">
+            <div className="page-name">
+              <div className="name">Product</div>
+            </div>
+            <div className="breadcrumbs"> <span>Home</span> / <span>Collection</span> </div>
+          </div>
+            <div className="preview"></div>
+          
         </div>
         <div className="center">
           <div className="responsive">
-            <div className="devices_wrap">
-              <div className="highlighter a"></div>
+            <div className="devices__wrap">
+              <div className={`highlighter ${highlightedDevice}`}></div>
               <div className="devices">
-                <div className="device a highlight "></div>
-                <div className="device b"></div>
-                <div className="device c"></div>
-                <div className="device d"></div>
+              {
+                devices.map(device=>(
+                  <div key={device} className={`device ${device} ${ (device===highlightedDevice?'highlighted':'')} `}
+                  onClick={() => toggleHighlight(device)}
+                  ></div>
+                ))
+              }
               </div>
+            </div>
+            <div className="device__size">
+              <div className="unit">1920 px</div>
             </div>
           </div>
           <div className="role"></div>
@@ -25,7 +48,22 @@ export function Header() {
           </div>
         </div>
         <div className="right">
+          <div className="git">
+            <div className="dropdown disabled repository ">
+              <div className="input">
+                jm-ecommerce
+              </div>
+              <div className="label">Repository</div>
+            </div>
+            <div className="dropdown branch">
+              <div className="input">
+                <span>fix/</span> product-option
+              </div>
+              <div className="label">Branch</div>
+
+            </div>
+          </div>
         </div>
       </header>
-  );
+  );  
 }
