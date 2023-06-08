fc3f213d39d9095b748983b16b31d4c7701cfda4
Abhay <abhaykvincent@gmail.com>
2023-06-08 18:13:19 -0400

    margin update at worIdk#0154

--- src/components/layoutInspector/LayoutInspector.tsx
+++ src/components/layoutInspector/LayoutInspector.tsx
@@ -17,53 +17,42 @@ export function LayoutInspector({layoutStyle,targetID} : LayoutInspectorProps) {
   const layoutTabs = ['block', 'flex', 'grid' , 'inline-block', 'inline', 'none']
   const [highlightedLayoutTab, setHighlightedLayoutTab] = useState(layoutStyle);
 
+  // Dcuent stylesheet
+  let styleSheet = document.styleSheets[0];
   // Functions
 
-  // 1. Toggle Layout Tab
-  // Description: toggles the highlighted layout tab based on the tab clicked, passed as a parameter
-  const toggleLayoutTab = (layoutTab:string) => {
-
-    // set the highlighted layout tab to the tab clicked
-    setHighlightedLayoutTab(layoutTab);
-  };
-
-
-  // LIFE CYCLE METHODS
-
-  // 1. useEffect - highlightedLayoutTab
-  // when the highlightedLayoutTab changes, update the layout style of the selected element
-  useEffect(() => {
-    // Get target element data-flow-id
-    const target = document.querySelector(`[data-flow-id="${targetID}"]`);
-    // set the layout style of the selected element to the highlighted layout tab
-
-    //target?.setAttribute('style', `display: ${highlightedLayoutTab}`);
-
-    // use newRule to create a new CSS rule for the selected element  by flow-id
-
-    const newRule = `[data-flow-id="${targetID}"] { display: ${highlightedLayoutTab} }`;
-    // create a new style element
-
-    // Get the document's stylesheet
-    let styleSheet = document.styleSheets[0];
-    
-    // Check if the rule already exists
+  const updateCSSRule = (targetSelector: string, highlightedLayoutTab: string) => {
     let ruleExists = false;
+    // Check if the rule already exists
     for (let i = 0; i < styleSheet.cssRules.length; i++) {
-        if (styleSheet.cssRules[i].cssText.startsWith(newRule.split(" ")[0])) {
+        if (styleSheet.cssRules[i].cssText.startsWith(targetSelector)) {
             ruleExists = true;
+            // Update the existing rule
             const rule=styleSheet.cssRules[i] as CSSStyleRule;
             rule.style.display=highlightedLayoutTab;
-            console.log('rule exists', newRule);
-            console.log(styleSheet.cssRules[i])
             break;
         }
     }
-    
-    // Insert the new CSS rule into the stylesheet if it doesn't already exist
+    // If the rule does not exist
     if (!ruleExists) {
-        styleSheet.insertRule(newRule, styleSheet.cssRules.length);
+      // Create a new rule
+      const newRule = `${targetSelector} { display: ${highlightedLayoutTab}; }`;
+      styleSheet.insertRule(newRule, styleSheet.cssRules.length);
     }
+}
+
+  // LIFE CYCLE METHODS
+
+  // useEffect - highlightedLayoutTab | When user changes the display value of the selected element.
+  /* 
+    When the highlightedLayoutTab changes: 
+    1. 
+  */
+  useEffect(() => {
+    // Get the target element selector
+    const targetSelector = `[data-flow-id="${targetID}"]`;
+    // Update the CSS rule
+    updateCSSRule(targetSelector, highlightedLayoutTab);
     
   }, [highlightedLayoutTab]);
 
@@ -82,7 +71,7 @@ export function LayoutInspector({layoutStyle,targetID} : LayoutInspectorProps) {
           {
             layoutTabs.map(tab=>(
               <div key={tab} className={`layout-tab ${tab} ${ (tab===highlightedLayoutTab?'highlighted':'')} `}
-              onClick={() => toggleLayoutTab(tab)}
+              onClick={() => setHighlightedLayoutTab(tab)}
               ></div>
             ))
           }
