5e8f355120f4e02941edd9e51d8d311f91c6dd57
Abhay <abhaykvincent@gmail.com>
2023-06-11 15:15:04 -0400

    Index JSX  refactor#30021

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import reportWebVitals from './reportWebVitals';
+
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import './index.scss';
 
 const container = document.getElementById('root')!;
