f716e4417618b350abcbe4a4eead8ddd40873b87
Abhay <abhaykvincent@gmail.com>
2023-06-11 15:32:17 -0400

    App test created

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { container } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+test('renders App component with Provider and store', () => {
+  const container = document.createElement('div');
+  container.setAttribute('id', 'root');
+  const root = createRoot(container);
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
   );
-  const appDiv = container.querySelector('.App');
-  expect(appDiv).toBeInTheDocument();
+  expect(store.getState().canvas).toBeDefined();
 });
