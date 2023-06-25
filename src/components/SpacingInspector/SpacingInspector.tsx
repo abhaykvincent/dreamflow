63fa8cf4128e62aed074a19aeb36232d784a3ccf
Abhay <abhaykvincent@gmail.com>
2023-06-25 01:07:17 -0400

    Fixed input sync with state

--- src/components/SpacingInspector/SpacingInspector.tsx
+++ src/components/SpacingInspector/SpacingInspector.tsx
@@ -6,6 +6,28 @@ interface SpacingInspectorProps {
 }
 let styleSheet = document.styleSheets[0];
 export default function SpacingInspector(targetID: SpacingInspectorProps) {
+  const [spacing, setSpacing] = useState({
+    padding: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    },
+    margin: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    },
+  });
+  const handleSpacingChange = (e: any) => {
+    const { name, value } = e.target;
+    const [type, direction] = name.split('-');
+    setSpacing((prevSpacing) => ({
+      ...prevSpacing,
+    }));
+  };
+
   return(
     <div className="spacing-inspector">
        <div className="panel-section__title">Spacing</div>
@@ -14,20 +36,20 @@ export default function SpacingInspector(targetID: SpacingInspectorProps) {
             <div className="spacing__wrap">
               <div className="spacing__label">Padding</div>
               <div className="spacing__input">
-                <input type="number" className="spacing__input__field" value='0' />
-                <input type="number" className="spacing__input__field" value='0' />
-                <input type="number" className="spacing__input__field" value='0' />
-                <input type="number" className="spacing__input__field" value='0' />
+                <input type="number" className="spacing__input__field" value={spacing.padding.top} onChange={handleSpacingChange} name="padding-top" />
+                <input type="number" className="spacing__input__field" value={spacing.padding.right} onChange={handleSpacingChange} name="padding-right" />
+                <input type="number" className="spacing__input__field" value={spacing.padding.bottom} onChange={handleSpacingChange} name="padding-bottom" />
+                <input type="number" className="spacing__input__field" value={spacing.padding.left} onChange={handleSpacingChange} name="padding-left" />
               </div>
             </div>
 
             <div className="spacing__wrap">
               <div className="spacing__label">Margin</div>
               <div className="spacing__input">
-                <input type="number" className="spacing__input__field" value='0' />
-                <input type="number" className="spacing__input__field" value='0' />
-                <input type="number" className="spacing__input__field" value='0' />
-                <input type="number" className="spacing__input__field" value='0' />
+                <input type="number" className="spacing__input__field" value={spacing.margin.top} onChange={handleSpacingChange} name="margin-top" />
+                <input type="number" className="spacing__input__field" value={spacing.margin.right} onChange={handleSpacingChange} name="margin-right" />
+                <input type="number" className="spacing__input__field" value={spacing.margin.bottom} onChange={handleSpacingChange} name="margin-bottom" />
+                <input type="number" className="spacing__input__field" value={spacing.margin.left} onChange={handleSpacingChange} name="margin-left" />
               </div>
             </div>
             
