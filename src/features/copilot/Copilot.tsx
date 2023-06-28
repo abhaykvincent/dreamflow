056cacee4eedf5ddcc8601df9077e5c350d22640
Abhay <abhaykvincent@gmail.com>
2023-06-27 23:51:21 -0400

    Copilot panel movable on header  click and drag

--- src/features/copilot/Copilot.tsx
+++ src/features/copilot/Copilot.tsx
@@ -1,10 +1,40 @@
 import './Copilot.scss';
+
 export function Copilot() {
+  const dragChatbox = (e: any) => {
+    console.log('clicked');
+    const chatbox = document.getElementsByClassName('chatbox')[0] as HTMLDivElement;
+    if (!chatbox) return;
+    const initialMousePositionX = e.clientX;
+    const initialMousePositionY = e.clientY;
+    const {top, left} = chatbox.getBoundingClientRect();
+    // on mouse move get the change in mouse position  and add it to the chatbox position
+    const handleMouseMove = (e: any) => {
+      console.log('dragging');
+      const changeInMousePositionX = e.clientX - initialMousePositionX;
+      const changeInMousePositionY = e.clientY - initialMousePositionY;
+      const newRight = left + changeInMousePositionX;
+      const newBottom = top + changeInMousePositionY;
+      console.log({changeInMousePositionX, changeInMousePositionY});
+      console.log({newRight, newBottom});
+      chatbox.setAttribute('style', `left: ${newRight}px; top: ${newBottom}px;`)
+
+    }
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    }
+    );
+
+  }
   return (
     <div className="copilot">
       <div className="chatbox">
 
-        <div className="chatbox__header">
+        <div className="chatbox__header"
+         onMouseDown={dragChatbox}
+        >
           <div className="chatbox__header__title">Copilot <span>Powery by OpenAI</span>
           </div>
           <div className="chatbox__header__buttons">
