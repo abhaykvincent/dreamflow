e0c885fcab5623eaadedf42502b2720e26300a2f
Abhay <abhaykvincent@gmail.com>
2023-07-03 02:00:07 -0400

    HTL, CSS, JS extraction from generative tokens

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -34,34 +34,6 @@ function gatherNodes(element: HTMLElement): NodeObj[] {
 // each node with html <div className="node" key={index}>{node.node.tagName}</div>
 
 
-const generateLayer = (value:any,level:number,currentLayer:any) => {
-  $(value).children().each(function(index:number, v:any) {
-
-    let lvl = level + 1;
-
-    let newLayer = document.createElement('div');
-    newLayer.className = 'layer';
-
-    let selectorClasses=$('.selected')[0].className.replace('selected','');
-
-    // if selectorClasses starts with flow, remove it
-    if(selectorClasses.startsWith('flow')) {
-      selectorClasses = selectorClasses.replace('flow','');
-      // all uppercase
-      selectorClasses = selectorClasses.toUpperCase();
-    }
-    console.log(selectorClasses)
-    //class name exept 'selected
-    // get tag name of the element
-    newLayer.innerHTML=`<div class="layer__tab">${v.tagName}<div/>`
-    
-    newLayer.dataset.layerId = $(v).data('flow-id');
-    newLayer.style.marginLeft = lvl*4 + 'px';
-    newLayer.style.borderLeft = lvl*0.5+'px solid rgba(255, 255, 255, 0.1)'
-    currentLayer.append(newLayer);
-    generateLayer(v,lvl++,newLayer);
-  });
-}
 
 export function Sidebar() {
   const canvasDOM = useSelector(selectCanvasDOM);
@@ -78,7 +50,6 @@ export function Sidebar() {
       setAllNodes(gatherNodes(canvas))
     }
 
-    generateLayer('#canvas',0,$('.nodes'));
   }, [canvasDOM]);
 
   const [activeSidebarTool, setActiveSidebarTool] = useState('elements');
